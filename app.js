8d14a9585dc83a4f47fa3620056e4f7564d32fa5
joelcorey <joelcorey@fastmail.com>
2019-04-01 05:53:44 -0700

    gitignore, stub app

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hi');
+'use-strict';
+
+const express = require('express');
+const app = express();
+
+app.listen(8000, () => {
+  console.log('Express server is listening on port 8000');
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+})
